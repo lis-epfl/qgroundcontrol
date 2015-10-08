4e4a1b35074fff0bd14ce028fc87eb7958852fd4
ndousse <nicolas.dousse@epfl.ch>
2015-10-08 17:46:27 +0200

    Sorting by system ID modulo 10

--- src/ui/mission/QGCSwarmControl.h
+++ src/ui/mission/QGCSwarmControl.h
@@ -1,3 +1,34 @@
+/*=====================================================================
+ 
+ QGroundControl Open Source Ground Control Station
+ 
+ (c) 2009 - 2015 QGROUNDCONTROL PROJECT <http://www.qgroundcontrol.org>
+ 
+ This file is part of the QGROUNDCONTROL project
+ 
+ QGROUNDCONTROL is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ QGROUNDCONTROL is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with QGROUNDCONTROL. If not, see <http://www.gnu.org/licenses/>.
+ 
+ ======================================================================*/
+
+// The above copyright block should be at the top of every file.
+
+/// @file
+///     @brief This file is used to control a group of muliple robot 
+///             having in mind the MAV'RIC framework. 
+///
+///     @author Nicolas Dousse <nic.dousse@gmail.com>
+
 #ifndef QGCSWARMCONTROL_H
 #define QGCSWARMCONTROL_H
 
@@ -24,46 +55,46 @@ public:
 
 public slots:
 
-	void textMessageReceived(UASInterface* uas, QString message);
-	void textEmit(int uasid, int component, int severity, QString message);
+    void textMessageReceived(UASInterface* uas, QString message);
+    void textEmit(int uasid, int component, int severity, QString message);
 
 private slots:
-    void continueAllButton_clicked();
-	void Return2startButton_clicked();
-	void launchScenario_clicked();
-	void autoLanding_clicked();
-	void autoTakeoff_clicked();
+    void continueAllButton_clicked(void);
+    void Return2startButton_clicked(void);
+    void launchScenario_clicked(void);
+    void autoLanding_clicked(void);
+    void autoTakeoff_clicked(void);
 
-	void startLogging_clicked();
-	void stopLogging_clicked();
+    void startLogging_clicked(void);
+    void stopLogging_clicked(void);
 
-	void UASCreated(UASInterface* uas);
-	void RemoveUAS(UASInterface* uas);
-	void ListWidgetClicked(QListWidgetItem* item);
+    void UASCreated(UASInterface* uas);
+    void RemoveUAS(UASInterface* uas);
+    void ListWidgetClicked(QListWidgetItem* item);
 
-	void newActiveUAS(UASInterface* uas);
+    void newActiveUAS(UASInterface* uas);
 
-	void refreshView();
+    void refreshView(void);
 
-	void setParameters_clicked();
+    void setParameters_clicked(void);
 
-	void startButton_clicked();
-	void stopButton_clicked();
+    void startButton_clicked(void);
+    void stopButton_clicked(void);
 
-	void armButton_clicked();
-	void disarmButton_clicked();
+    void armButton_clicked(void);
+    void disarmButton_clicked(void);
 
-	void selectButton_clicked();
+    void selectButton_clicked(void);
 
-	void remoteItem_clicked(QListWidgetItem* item);
+    void remoteItem_clicked(QListWidgetItem* item);
 
-	void remoteButton_clicked();
+    void remoteButton_clicked(void);
 
-	void setMode(int mode);
+    void setMode(int mode);
 
-	void sendNewHomePosition();
+    void sendNewHomePosition(void);
 
-	void strategyLaunchClicked();
+    void strategyLaunchClicked(void);
 
 private:
     Ui::QGCSwarmControl *ui;
@@ -73,29 +104,32 @@ private:
     int wptReachedCnt;
 
 protected:
-	MAVLinkProtocol* mavlink;     ///< Reference to the MAVLink instance
-	UASInterface *uas;
-	QList<UASInterface*> UASlist;
+    MAVLinkProtocol* mavlink;     ///< Reference to the MAVLink instance
+    UASInterface *uas;
+    QList<UASInterface*> UASlist;
 
-	QMap<UASInterface*,QListWidgetItem*> uasToItemMapping;
-	QMap<QListWidgetItem*,UASInterface*> itemToUasMapping;
+    QMap<UASInterface*,QListWidgetItem*> uasToItemMapping;
+    QMap<QListWidgetItem*,UASInterface*> itemToUasMapping;
 
-	QMap<UASInterface*,QListWidgetItem*> uasToItemRemote;
-	QMap<QListWidgetItem*,UASInterface*> itemToUasRemote;	
+    QMap<UASInterface*,QListWidgetItem*> uasToItemRemote;
+    QMap<QListWidgetItem*,UASInterface*> itemToUasRemote;    
 
-	UASInterface *uas_previous;
+    QMap<QListWidgetItem*,int> itemToID;
+    QMap<QListWidgetItem*,int> itemToIDRemote;
 
-	QTimer updateTimer; ///< Only update at 1 Hz to not overload the GUI
-	static const unsigned int updateInterval; ///< The update interval of the refresh function
+    UASInterface *uas_previous;
 
-	bool all_selected;
+    QTimer updateTimer; ///< Only update at 1 Hz to not overload the GUI
+    static const unsigned int updateInterval; ///< The update interval of the refresh function
 
-	bool mode_init;
+    bool all_selected;
+
+    bool mode_init;
     int modeIdx;                    ///< Current uas mode index
- 	QList<AutoPilotPluginManager::FullMode_t>  _modeList;  ///< Mode list for the current UAS
+     QList<AutoPilotPluginManager::FullMode_t>  _modeList;  ///< Mode list for the current UAS
 
 signals:
-	void uasTextReceived(UASInterface* uas, QString message);
+    void uasTextReceived(UASInterface* uas, QString message);
 
 };
 
